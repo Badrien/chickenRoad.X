51d7ab54015d31a04eb6daedb1716aba6267136e
Badrien <adrienboppe@gmail.com>
2018-10-23 14:37:22 +0200

    creation du main

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: adrie
+ *
+ * Created on 23 octobre 2018, 14:23
+ */
+
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void) {
+    printf("Welcome to Chicken Road, the best ");
+}
